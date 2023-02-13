9cab75cc4c69e9722a0383538c82e9c82fd63107
Afeef <86735357+AfeefMM@users.noreply.github.com>
2023-02-13 13:35:10 +0400

    new branch commit with splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:inventory_mgmt/utils/colours.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import 'home/code_entry.dart';
 import 'home/description.dart';
@@ -16,12 +18,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inventory Checker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CodeEntryPage(),
-    );
+    return SplashScreen(
+        seconds: 5,
+        navigateAfterSeconds: CodeEntryPage(),
+        image: new Image.asset('assets/Giordano-logo 1.png'),
+        photoSize: 100.0,
+        backgroundColor: AppColours.mainColor,
+        styleTextUnderTheLoader: new TextStyle(),
+        loaderColor: AppColours.mainColor);
   }
 }
